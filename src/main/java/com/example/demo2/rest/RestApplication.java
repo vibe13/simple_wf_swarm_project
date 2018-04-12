c2e7b57aeb813563de6b046a6ee83fa5a2c17525
Andrea Vibelli <andrea.vibelli@gmail.com>
2018-04-12 14:56:19 +0200

    Added fix if Application class extends some methods (see https://issues.jboss.org/browse/SWARM-1667)

--- src/main/java/com/example/demo2/rest/RestApplication.java
+++ src/main/java/com/example/demo2/rest/RestApplication.java
@@ -1,10 +1,24 @@
 package com.example.demo2.rest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
 
 @ApplicationPath("/")
 public class RestApplication extends Application {
 
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        addEndpoints(resources);
+        return resources;
+    }
+
+    private void addEndpoints(Set<Class<?>> resources) {
+        resources.add(HelloWorldEndpoint.class);
+    }
 }
